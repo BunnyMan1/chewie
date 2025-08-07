634eb890c23326e5fb398daf46f12d7c61a5eb34
ranadeepmedagone <ranadeep.medagone@narayanagroup.com>
2025-08-07 14:56:37 +0530

    Update material_controls.dart

--- lib/src/material/material_controls.dart
+++ lib/src/material/material_controls.dart
@@ -72,17 +72,18 @@ class _MaterialControlsState extends State<MaterialControls>
         absorbing: notifier.hideStuff,
         child: Stack(
           children: [
-            if (_displayBufferingIndicator || _latestValue.isBuffering)
-              Center(
-                child: _buildIconbutton(
-                  onTap: () {},
-                  icon: null,
-                  alwayShow: true,
-                  iconWidget: const CircularProgressIndicator(
-                    valueColor: AlwaysStoppedAnimation(Colors.white),
-                  ),
-                ),
-              )
+            if (_displayBufferingIndicator)
+              _chewieController?.bufferingBuilder?.call(context) ??
+                  Center(
+                    child: _buildIconbutton(
+                      onTap: () {},
+                      icon: null,
+                      alwayShow: true,
+                      iconWidget: const CircularProgressIndicator(
+                        valueColor: AlwaysStoppedAnimation(Colors.white),
+                      ),
+                    ),
+                  )
             else
               _buildHitArea(),
             Column(
