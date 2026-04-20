11869c9e8d76caedf1061586d2f7c0353fee7244
Ravalika Madari <ravalika.madari@narayanagroup.com>
2026-04-20 15:46:02 +0530

    Update cupertino_controls.dart

--- lib/src/cupertino/cupertino_controls.dart
+++ lib/src/cupertino/cupertino_controls.dart
@@ -538,9 +538,12 @@ class _CupertinoControlsState extends State<CupertinoControls>
       final externalToggle = chewieController.onExternalFullScreenToggle;
       if (externalToggle != null) {
         final entering = !chewieController.isFullScreen;
-        chewieController.enterFullScreen(notify: false);
-        if (!entering) chewieController.exitFullScreen();
-        externalToggle(!chewieController.isFullScreen);
+        if (entering) {
+          chewieController.enterFullScreen(notify: false);
+        } else {
+          chewieController.exitFullScreen(notify: false);
+        }
+        externalToggle(entering);
       } else {
         chewieController.toggleFullScreen();
       }
